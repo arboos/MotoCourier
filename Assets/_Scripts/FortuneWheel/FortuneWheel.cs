cf49aa3da6c8417a446f3e959287ff888a9a72ff
DIImoNn22 <112386300+DIImoNn22@users.noreply.github.com>
2024-07-26 15:23:36 -0600

    Dima Scene

---
+++ Assets/_Scripts/FortuneWheel/FortuneWheel.cs
@@ -0,0 +1,105 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FortuneWheel : MonoBehaviour
+{
+    [SerializeField] private Image wheel;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] private float rotationTimeMaxSpeed;
+    [SerializeField] private float accelerationTime;
+    [SerializeField] private int numberOfSpins;
+    [SerializeField] private bool randomOptionForSpin;
+    [SerializeField] private List<Prize> prizes;
+
+    private bool isSpin = false;
+    private float slowdownTime;
+    private float randomAngle = 0f;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isSpin) 
+        {
+            StartCoroutine(SpinWheel());
+        }
+
+        /*if (Input.GetKeyDown(KeyCode.X))
+        {
+            for (int i = 0; i < 10000; i++)
+            {
+                int index = getRandomPrize();
+                Debug.Log("WIN : " + prizes[index].name + " | Index = " + index);
+            }
+        }*/
+    }
+
+    IEnumerator SpinWheel() 
+    {
+        if (randomOptionForSpin)
+        {
+            setRandomOptions();
+        }
+        setWin();
+        isSpin = true;
+        float elapsedTime = 0f;
+        float rotSpeed;
+        while (elapsedTime < accelerationTime)
+        {
+            rotSpeed = Mathf.Lerp(0, rotationSpeed, elapsedTime / accelerationTime);
+            wheel.transform.rotation *= Quaternion.Euler(0, 0, rotSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        elapsedTime = 0f;
+        while (elapsedTime < rotationTimeMaxSpeed)
+        {
+            wheel.transform.rotation *= Quaternion.Euler(0, 0, rotationSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+
+        float distance = (numberOfSpins * 360f) + randomAngle - wheel.transform.rotation.eulerAngles.z;
+        slowdownTime = (2 * distance) / rotationSpeed;
+
+        elapsedTime = 0f;
+        while (elapsedTime < slowdownTime)
+        {
+            rotSpeed = Mathf.Lerp(rotationSpeed, 0, elapsedTime / slowdownTime);
+            wheel.transform.rotation *= Quaternion.Euler(0, 0, rotSpeed * Time.deltaTime);
+            elapsedTime += Time.deltaTime;
+            yield return null;
+        }
+        
+        isSpin = false;
+    }
+
+    private void setWin() 
+    {
+        int randomSector = getRandomPrize();
+        float maxAngel = 360f / prizes.Count * (randomSector + 1);
+        float minAngel = 360f / prizes.Count * randomSector;
+        randomAngle = Random.Range(minAngel + 8, maxAngel - 8);
+    }
+
+    private int getRandomPrize()
+    {
+        int randomSector = Random.Range(0, prizes.Count);
+        if (prizes[randomSector].wight <= Random.Range(0f, 1f))
+        {
+            return getRandomPrize();
+        }
+        Debug.Log(randomSector);
+        return randomSector;
+    }
+
+    private void setRandomOptions()
+    {
+        rotationSpeed = Random.Range(250,1000);
+        rotationTimeMaxSpeed = Random.Range(0, 1.2f);
+        accelerationTime = Random.Range(0, 4f);
+        numberOfSpins = Random.Range(2, 6);
+    }
+
+}
---
+++ Assets/_Scripts/FortuneWheel/Prize.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class Prize
+{
+    public string name;
+    [Range(0, 1)] public float wight;
+}
