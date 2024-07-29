e581d3f99128280e222810b65cd01272558233c9
arboos <151074202+arboos@users.noreply.github.com>
2024-07-29 18:59:39 +0300

    enemy spawner

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -7,17 +7,18 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager Instance { get; private set; }
 
+    public Transform player;
     public Transform point;
     public List<Transform> points;
 
 
-    private void Start()
-    {
-        for (int i = 0; i < point.childCount; i++)
-        {
-            points.Add(point.GetChild(i));
-        }
-    }
+    // private void Start()
+    // {
+    //     for (int i = 0; i < point.childCount; i++)
+    //     {
+    //         points.Add(point.GetChild(i));
+    //     }
+    // }
 
     private void Awake()
     {
